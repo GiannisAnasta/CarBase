6b3576d88bf9164dacd7bc55efa156f3df6dfe13
giannis <giannakos96@hotmail.com>
2016-08-23 18:50:40 +0300

    case insesnitivity works.

--- src/main/java/view/CompanyTableFilter.java
+++ src/main/java/view/CompanyTableFilter.java
@@ -2,6 +2,7 @@ package view;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Locale;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import model.Company;
@@ -18,6 +19,19 @@ public class CompanyTableFilter implements Serializable {
 
     public void setFilteredCompanies(List<Company> filteredCompanies) {
         this.filteredCompanies = filteredCompanies;
+    }
+
+    public boolean filterExact(Object value, Object filter, Locale locale) {
+        String filterText = (filter == null) ? null : filter.toString();
+        if (filterText == null || filterText.equals("")) {
+            return true;
+        }
+
+        if (value == null) {
+            return false;
+        }
+
+        return value.toString().equalsIgnoreCase(filterText);
     }
 
 }
