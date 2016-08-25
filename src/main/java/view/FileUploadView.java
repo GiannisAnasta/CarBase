f8c5b451d54f8724fc81f4ab45ce1fa80b127d7e
giannis <giannakos96@hotmail.com>
2016-08-25 15:14:43 +0300

    Faces message for dublicated companies on file upload is ready.

--- src/main/java/view/FileUploadView.java
+++ src/main/java/view/FileUploadView.java
@@ -5,10 +5,14 @@ import fileupload.IndefiniteData;
 import fileupload.UploadedFileDataReader;
 import fileupload.validation.exceptions.UploadedFileReadException;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.facelets.FaceletContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import model.Company;
@@ -38,9 +42,41 @@ public class FileUploadView implements Serializable {
             Logger.getLogger(FileUploadView.class.getName()).log(Level.SEVERE, null, ex);
         }
         List<Company> buildedCompanies = CompanyBuilder.buildCompanies(dataFromUploadedFile);
+
         List<Company> uniqueCompaniesFromFile = CompaniesUtil.getUniqueCompanies(buildedCompanies);
-        manager.addcompanies(uniqueCompaniesFromFile);
-        System.out.println("finish");
+        if (buildedCompanies.size() != uniqueCompaniesFromFile.size()) {
+            List<Company> dublicates = new ArrayList<>(buildedCompanies);
+            dublicates.removeAll(uniqueCompaniesFromFile);
+            FacesMessage msg = new FacesMessage(buildedCompanies.size() - uniqueCompaniesFromFile.size() + " dublicates detected inside the file and removed");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+            for (Company c : dublicates) {
+                FacesMessage msg1 = new FacesMessage("ignored <" + c.getName() + "> company");
+                FacesContext.getCurrentInstance().addMessage(null, msg1);
+            }
+        }
+
+        ArrayList<Company> DBlist = manager.getList();
+        int size = uniqueCompaniesFromFile.size();
+        List<Company> fileNewItemsForDB = new ArrayList<>(uniqueCompaniesFromFile);
+        fileNewItemsForDB.removeAll(DBlist);
+
+        if (size != fileNewItemsForDB.size()) {
+            FacesMessage msg = new FacesMessage(size - fileNewItemsForDB.size() + " companies already exists! Dublicates removed.");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+
+            List<Company> fileDBDublicates = new ArrayList<>(uniqueCompaniesFromFile);
+            fileDBDublicates.removeAll(fileNewItemsForDB);
+            for (Company c : fileDBDublicates) {
+                FacesMessage msg1 = new FacesMessage("ignored <" + c.getName() + "> company");
+                FacesContext.getCurrentInstance().addMessage(null, msg1);
+            }
+        }
+
+        manager.addcompanies(fileNewItemsForDB);
+        if (!fileNewItemsForDB.isEmpty()) {
+            FacesMessage msg = new FacesMessage(fileNewItemsForDB.size() + " companies uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
     }
 
 }
