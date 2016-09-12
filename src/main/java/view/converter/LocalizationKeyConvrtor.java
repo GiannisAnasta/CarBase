64aa147e441ff2b67ea588834b9769402daa8afd
giannis <giannakos96@hotmail.com>
2016-09-12 15:07:46 +0300

    Enum FormatOfData localized with LocalizationKeyConvrtor.toLocalizationKey(FormatOfData fd)

---
+++ src/main/java/view/converter/LocalizationKeyConvrtor.java
@@ -0,0 +1,22 @@
+package view.converter;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import view.FormatOfData;
+
+@Named
+@ApplicationScoped
+public class LocalizationKeyConvrtor {
+
+    public String toLocalizationKey(FormatOfData fd) {
+        switch (fd) {
+            case EMPTY_LINE_SEPARATED:
+                return "emptyLineSeparated";
+            case NEW_LINE_SEPARATED:
+                return "newLineSeparated";
+            default:
+                return "unknown key";
+        }
+    }
+
+}
