60b8b43a29593b60b83055bb48cf2ea74528c8fd
giannis <giannakos96@hotmail.com>
2016-09-22 18:10:35 +0300

    confirm and decline on preview db #28 done with some localization without Russian.

--- src/main/java/view/converter/ListStringConverter.java
+++ src/main/java/view/converter/ListStringConverter.java
@@ -1,13 +1,10 @@
 package view.converter;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
-import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 
 @FacesConverter("ListStringConverter")
